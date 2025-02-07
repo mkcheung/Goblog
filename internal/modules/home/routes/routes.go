75dd9f4701eafb43ef07f7e4e8f810caf9e26b9a
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-02-06 23:25:27 -0800

    install an html package

--- internal/modules/home/routes/routes.go
+++ internal/modules/home/routes/routes.go
@@ -1,18 +1,22 @@
 package routes
 
 import (
+	"blog/pkg/html"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 func Routes(router *gin.Engine) {
 
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message":  "pong",
-			"app name": viper.Get("App.Name"),
+		html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
+			"title": "Home page",
+		})
+	})
+	router.GET("/about", func(c *gin.Context) {
+		html.Render(c, http.StatusOK, "modules/home/html/about", gin.H{
+			"title": "About page",
 		})
 	})
 }
