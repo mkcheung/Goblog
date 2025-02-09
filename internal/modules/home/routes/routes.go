6b61a945b962ac44eaf9201f02edb7805bfdb30b
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-02-09 00:09:55 -0800

    add home controller

--- internal/modules/home/routes/routes.go
+++ internal/modules/home/routes/routes.go
@@ -1,17 +1,12 @@
 package routes
 
 import (
-	"blog/pkg/html"
-	"net/http"
+	homeCtrl "blog/internal/modules/home/controllers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(router *gin.Engine) {
-
-	router.GET("/", func(c *gin.Context) {
-		html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
-			"title": "Home page",
-		})
-	})
+	homeController := homeCtrl.New()
+	router.GET("/", homeController.Index)
 }
